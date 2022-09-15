303e6ff7363f0fa24c8fa30832ffff22c585fcee
P.Nam <nampp@inspirelab.io>
2022-09-15 13:59:14 +0700

    Initial Commit

---
+++ ViperDemo/ViperDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViperDemoApp.swift
+//  ViperDemo
+//
+//  Created by P.Nam on 15/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct ViperDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
